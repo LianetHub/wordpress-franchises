1782da983ac34d1bb9587d95231b8dbbf23d00d9
lianet <jack.tolmak@yandex.ru>
2026-05-21 20:04:38 +0300

    add favicon

--- header.php
+++ header.php
@@ -6,7 +6,15 @@
     <meta name="format-detection" content="telephone=no">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <!-- favicon -->
+    <?php $assets_uri = get_template_directory_uri() . '/assets'; ?>
+    <link rel="icon" type="image/png" href="<?php echo esc_url($assets_uri . '/favicon-96x96.png'); ?>" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="<?php echo esc_url($assets_uri . '/favicon.svg'); ?>" />
+    <link rel="shortcut icon" href="<?php echo esc_url($assets_uri . '/favicon.ico'); ?>" />
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo esc_url($assets_uri . '/apple-touch-icon.png'); ?>" />
+    <meta name="apple-mobile-web-app-title" content="Франшизы" />
+    <link rel="manifest" href="<?php echo esc_url($assets_uri . '/site.webmanifest'); ?>" />
     <!-- favicon -->
 
     <?php wp_head(); ?>
